4946fcca77ac1e7fe0431a918aec1fd267daafd0
vladimir.makhmutov <vladimir.makhmutov@indusoft.ru>
2021-07-02 21:58:13 +0700

    Закончил формирование API

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // mongoURI: 'mongodb://vladilen:123456@ds111410.mlab.com:11410/fullstack'
+  // mongoURI: 'mongodb+srv://vladimir:FUYhEJPeQs3gQZ1F@cluster0.givqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://vladimir:FUYhEJPeQs3gQZ1F@cluster0.givqc.mongodb.net/MEANDB?retryWrites=true&w=majority',
+  jwt: 'dev-jwt'
+}
